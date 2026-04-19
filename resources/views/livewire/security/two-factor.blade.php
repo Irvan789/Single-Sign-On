9fddbea169f47d9334a3b93b4a4c782dcdbf14e6
Irvan789 <irvantonnywidjaya789@gmail.com>
2026-04-19 13:11:33 +0700

    Update style

--- resources/views/livewire/security/two-factor.blade.php
+++ resources/views/livewire/security/two-factor.blade.php
@@ -17,7 +17,7 @@
         </div>
 
         <div class="flex flex-col gap-4 sm:col-span-2">
-          <div class="grid gap-2 font-mono text-base/5 font-medium sm:grid-cols-2">
+          <div class="text-[0.9375rem]/4.5 grid gap-2 font-mono font-normal sm:grid-cols-2">
             @if (filled($recoveryCodes))
               @foreach ($recoveryCodes as $code)
                 <span>
@@ -28,7 +28,7 @@
           </div>
 
           <x-button
-            class="max-w-30 ml-auto w-full text-xs/3"
+            class="xs:max-w-30 ml-auto w-full text-xs/3"
             wire:click="regenerateRecoveryCodes"
             wire:loading.attr="disabled"
           >
@@ -52,7 +52,7 @@
         </div>
 
         <x-button
-          class="max-w-30 ml-auto w-full text-xs/3"
+          class="xs:max-w-30 ml-auto w-full text-xs/3"
           wire:click="disableTwoFactor"
           wire:loading.attr="disabled"
         >
