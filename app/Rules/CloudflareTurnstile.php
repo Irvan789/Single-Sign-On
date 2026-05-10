c179612c5ff39de59775f8cf864c65502f1e6c78
Irvan789 <irvantonnywidjaya789@gmail.com>
2026-05-10 15:57:01 +0700

    Remove unused imports

--- app/Rules/CloudflareTurnstile.php
+++ app/Rules/CloudflareTurnstile.php
@@ -5,7 +5,6 @@ namespace App\Rules;
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Translation\PotentiallyTranslatedString;
 
 class CloudflareTurnstile implements ValidationRule
