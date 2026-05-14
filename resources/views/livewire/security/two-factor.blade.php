87f413176e48f2c074ac6585c9460d1de0c1cb06
Irvan789 <irvantonnywidjaya789@gmail.com>
2026-05-14 11:09:31 +0700

    Refactor Two-Factor Authentication UI and update text styles

--- resources/views/livewire/security/two-factor.blade.php
+++ resources/views/livewire/security/two-factor.blade.php
@@ -1,23 +1,19 @@
 <x-contents>
-  <div class="text-lg/4.5 font-bold">
-    Two-Factor Authentication
-  </div>
-
   @if ($canManageTwoFactor)
     @if ($twoFactorEnabled)
       <div class="grid grid-cols-1 gap-4 sm:grid-cols-3">
-        <div class="block">
-          <div class="text-base/4 font-bold">
-            Recovery Codes
+        <div class="-mt-px block space-y-0.5">
+          <div class="text-lg/5.5 font-bold">
+            Two-Factor Recovery Codes
           </div>
 
-          <div class="text-[0.9375rem]/4.5 mt-1 sm:mt-2">
+          <div class="text-smd">
             Keep your recovery codes safely for unauthorization access
           </div>
         </div>
 
         <div class="flex flex-col gap-4 sm:col-span-2">
-          <div class="text-[0.9375rem]/4.5 grid gap-2 font-mono font-normal sm:grid-cols-2">
+          <div class="text-smd grid gap-2 font-mono font-normal sm:grid-cols-2">
             @if (filled($recoveryCodes))
               @foreach ($recoveryCodes as $code)
                 <span>
@@ -40,12 +36,12 @@
       <x-separator />
 
       <div class="flex flex-col justify-between gap-4 md:flex-row md:items-center">
-        <div class="block">
-          <div class="text-lg/4.5 font-bold">
+        <div class="-mt-px block space-y-0.5">
+          <div class="text-lg/5.5 font-bold">
             Disable Two-Factor Authentication
           </div>
 
-          <div class="text-[0.9375rem]/4.5 mt-1 text-[#b85450]">
+          <div class="text-smd text-[#b85450]">
             When you disable two-factor authentication, you will be never prompted again for entering authentication
             code.
           </div>
@@ -60,57 +56,72 @@
         </x-button>
       </div>
     @else
-      <div class="flex flex-col gap-1 text-base/5 font-bold">
-        Setup authenticator app
-
-        <div class="text-sm/4.5 font-normal">
-          Use a phone app like Authy, Google Authenticator, or etc. to get 2FA codes when prompted.
-        </div>
-      </div>
-
-      <div class="flex flex-col gap-2">
-        <div class="text-base/4 font-bold">
-          Scan QR Code
-        </div>
-
-        @if (empty($qrCodeSvg))
-          Loading QR Code
-        @else
-          {!! $qrCodeSvg !!}
-
-          <div class="text-[0.9375rem]/4.5">
-            Unable to scan?, enter this code:
-
-            <span class="font-bold">
-              @if (empty($manualSetupKey))
-                Loading 2FA Key
-              @else
-                {{ $manualSetupKey }}
-              @endif
-            </span>
-
+      <div class="grid grid-cols-1 gap-4 sm:grid-cols-3">
+        <div class="-mt-px block space-y-0.5">
+          <div class="text-lg/5.5 font-bold">
+            Enable Two-Factor Authentication
           </div>
-        @endif
+
+          <div class="text-smd">
+            Follow the step to enable Two-Factor Authentication.
+          </div>
+        </div>
+
+        <div class="flex flex-col gap-4 sm:col-span-2">
+          <div class="block space-y-0.5">
+            <div class="text-base/5 font-bold">
+              Setup authenticator app
+            </div>
+
+            <div class="text-smd">
+              Use a phone app like Authy, Google Authenticator, or etc. to get 2FA codes when prompted.
+            </div>
+          </div>
+
+          <div class="flex flex-col gap-2">
+            <div class="text-base/5 font-medium">
+              Scan QR Code
+            </div>
+
+            @if (empty($qrCodeSvg))
+              Loading QR Code
+            @else
+              {!! $qrCodeSvg !!}
+
+              <div class="text-smd">
+                Unable to scan?, enter this code:
+
+                <span class="font-bold">
+                  @if (empty($manualSetupKey))
+                    Loading 2FA Key
+                  @else
+                    {{ $manualSetupKey }}
+                  @endif
+                </span>
+              </div>
+            @endif
+          </div>
+
+          <x-form
+            class="sm:col-span-2"
+            wire:submit="enableTwoFactor"
+          >
+            <x-input-text
+              label="Verify the code from phone app"
+              type="number"
+              wire:model="code"
+            />
+
+            <x-button
+              type="submit"
+              class="xs:max-w-30 ml-auto w-full text-xs/3"
+              x-bind:disabled="$wire.code.length < 6"
+            >
+              Verify
+            </x-button>
+          </x-form>
+        </div>
       </div>
-
-      <x-form
-        class="xs:max-w-84 xs:flex-row flex w-full flex-col items-end gap-x-2 gap-y-4"
-        wire:submit="enableTwoFactor"
-      >
-        <x-input-text
-          label="Verify the code from phone app"
-          type="number"
-          wire:model="code"
-        />
-
-        <x-button
-          type="submit"
-          class="xs:max-w-20 w-full text-sm/4"
-          x-bind:disabled="$wire.code.length < 6"
-        >
-          Verify
-        </x-button>
-      </x-form>
     @endif
   @endif
 </x-contents>
