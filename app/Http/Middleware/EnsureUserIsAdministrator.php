c681e8bbd3458f5776c96b7dbaae82aa60eba3f4
Irvan789 <irvantonnywidjaya789@gmail.com>
2026-06-01 11:31:50 +0700

    Refactor user role check

--- app/Http/Middleware/EnsureUserIsAdministrator.php
+++ app/Http/Middleware/EnsureUserIsAdministrator.php
@@ -15,7 +15,7 @@ class EnsureUserIsAdministrator
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if ($request->user()->role != 'admin') {
+        if ($request->user()->isAdmin()) {
             return abort(404);
         }
 
