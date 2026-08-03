53cb7b73ec35aa52d2011eae88094996271160bd
Irvan789 <irvantonnywidjaya789@gmail.com>
2026-08-04 05:48:17 +0700

    chore: update configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,27 @@
 import { defineConfig } from "vite"
 
-import laravel from "laravel-vite-plugin"
 import tailwindcss from "@tailwindcss/vite"
+import laravel from "laravel-vite-plugin"
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ["resources/css/app.css", "resources/ts/app.ts"],
-      refresh: true
-    }),
-    tailwindcss()
-  ],
-  server: {
-    cors: true,
-    watch: {
-      ignored: ["**/storage/framework/views/**"]
+    plugins: [
+        laravel({
+            input: [
+                "resources/css/app.css", 
+                "resources/ts/app.ts"
+            ],
+            refresh: true
+        }),
+        tailwindcss()
+    ],
+    server: {
+        watch: {
+            ignored: [
+                "**/.git/**",
+                "**/bootstrap/cache/**",
+                "**/storage/**",
+                "**/vendor/**",
+            ]
+        }
     }
-  }
 })
