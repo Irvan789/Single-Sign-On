8ca98bfeb031f47c4151d3fe1d49dff0f2ae054c
Irvan789 <irvantonnywidjaya789@gmail.com>
2026-08-04 15:26:12 +0700

    fix: update user route to use correct controller method

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 use Laravel\Passport\Http\Middleware\CheckToken;
 
 Route::middleware(['auth:api', CheckToken::using('user')])->group(function () {
-    Route::get('/user', [UserController::class, 'getUserData']);
+    Route::get('/user', [UserController::class, 'user']);
 });
