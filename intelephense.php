e5acba5dba0e35d48c971cb41af5539f00bd3690
irvan789 <irvantonnywidjaya789@gmail.com>
2026-08-10 02:11:05 +0700

    feat: add View interface with layout method for rendering views

---
+++ intelephense.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Illuminate\Contracts\View;
+
+use Illuminate\Contracts\Support\Renderable;
+
+interface View extends Renderable
+{
+    /**
+     * @return static
+     */
+    public function layout(string $view, ?array $data = []);
+}
