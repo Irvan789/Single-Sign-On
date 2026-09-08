4e263b9a20f6730122d1e824b3048d4aa0e93e83
irvan789 <irvantonnywidjaya789@gmail.com>
2026-09-09 00:47:06 +0700

    fix: ensure consistent button attributes merging in button component

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,5 +1,5 @@
 <button
-    {{ $attributes->except('testing')->merge(['type' => 'button', 'data-test'=> app()->environment('testing') && $attributes->has('testing') ? $testing : null])->twMerge('rounded px-3 py-2.5 text-sm/4 font-medium transition-colors duration-300', $attributes->has('variant') && $variant == 'danger' ? 'text-[#a8503d] inset-ring inset-ring-[#a8503d]/40 hover:bg-[#f5e9e4]' : 'bg-[#6b5a46] text-[#f0ede8] hover:bg-[#544636] disabled:bg-[#897967] disabled:hover:bg-[#897967]') }}
+    {{ $attributes->except('testing')->merge(['type' => 'button', 'data-test'=> app()->environment('testing') && $attributes->has('testing') ? $testing : null])->twMerge('rounded px-3 py-2.5 text-sm/4 font-medium transition-colors duration-300', $attributes->has('variant') && $variant == 'danger' ? 'text-[#a8503d] inset-ring inset-ring-[#a8503d]/40 hover:bg-[#f5e9e4] disabled:bg-[#f5e9e4]' : 'bg-[#6b5a46] text-[#f0ede8] hover:bg-[#544636] disabled:bg-[#897967] disabled:hover:bg-[#897967]') }}
 >
     {{ $slot }}
 </button>
